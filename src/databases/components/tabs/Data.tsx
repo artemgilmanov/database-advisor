0cbda97c7c86e2245601e7430e887d5a5cc704d1
Artem Gilmanov <artem.gilmanov@mesalvo.com>
2024-10-14 22:23:35 +0200

    Data component Dispatched addDataStructure.

--- src/databases/components/tabs/Data.tsx
+++ src/databases/components/tabs/Data.tsx
@@ -1,20 +1,42 @@
-import Dropdown from "@/shared/components/Dropdown";
-import {getEnumValues} from "@/shared/domain/helpers";
-import {DataStructures} from "@/databases/domain/types";
-import * as React from "react";
+import { useState } from 'react';
+import Dropdown from '@/shared/components/Dropdown';
+import { getEnumValues } from '@/shared/domain/helpers';
+import { DataStructures } from '@/databases/domain/types';
+import { useDispatch } from 'react-redux';
+import { addDataStructure } from '@/redux/DataStructure/actionCreators';
+import { SelectChangeEvent } from '@mui/material/Select';
 
 export default function Data() {
-    return <>
-        <Dropdown multiple options={getEnumValues(DataStructures)} label="Data Structure" />
+  const dispatch = useDispatch();
 
-        <Dropdown multiple options={[
-            'Financial',
-            'JSON',
-            'XML',
-            'IP',
-            'Spatial',
-            'Time Series'
-        ]} label="Supported Data Types" />
+  const [selectedValue, setSelectedValue] = useState<string>(
+    getEnumValues(DataStructures)[0]
+  );
 
-    </>;
+  const handleChange = (event: SelectChangeEvent) => {
+    const newValue = event.target.value;
+    setSelectedValue(newValue);
+
+    dispatch(
+      addDataStructure({ dataStructure: newValue, supportedDataTypes: '' })
+    );
+  };
+
+  return (
+    <>
+      <Dropdown
+        multiple={false}
+        options={getEnumValues(DataStructures)}
+        label='Data Structure'
+        handleChange={handleChange}
+        value={selectedValue}
+      />
+
+      <Dropdown
+        multiple
+        options={['Financial', 'JSON', 'XML', 'IP', 'Spatial', 'Time Series']}
+        label='Supported Data Types'
+      />
+    </>
+  );
 }
