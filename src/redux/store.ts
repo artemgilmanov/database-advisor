7e261f6527bddee528666eae5ed1d92e20fb8409
Artem Gilmanov <artem.gilmanov@mesalvo.com>
2024-10-15 22:22:06 +0200

    Refactored store to make it visible for Redux tooling in browser.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import dataStructureReducer from './DataStructure/reducer';
 
-export default configureStore({
+export const store = configureStore({
   reducer: {
     dataStructure: dataStructureReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
