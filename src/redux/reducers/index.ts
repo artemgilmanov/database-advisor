54c0b18f9f3d9383381263d93ba1b55006f62cd4
Artem Gilmanov <artem.gilmanov@mesalvo.com>
2024-10-19 17:22:44 +0200

    Added consistency reducer to rootReducer

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { dataStructureReducer } from './dataStructureReducer';
+import { consistencyReducer } from './consistencyReducer';
 
 export const rootReducer = combineReducers({
   dataStructure: dataStructureReducer,
+  consistency: consistencyReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
