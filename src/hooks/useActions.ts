8fb995bb616366032792d3600150218afdd90a4e
Artem Gilmanov <artem.gilmanov@mesalvo.com>
2024-10-19 17:20:55 +0200

    useActions binded all action creators.

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -1,8 +1,8 @@
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as DataStructureActionCreators from '../redux/actionCreators/dataStructureActionCreators';
+import ActionCreators from '../redux/actionCreators';
 
 export const useActions = () => {
   const dispatch = useDispatch();
-  return bindActionCreators(DataStructureActionCreators, dispatch);
+  return bindActionCreators(ActionCreators, dispatch);
 };
---
+++ src/redux/actionCreators/index.ts
@@ -0,0 +1,7 @@
+import * as DataStructureCreators from './dataStructure';
+import * as ConsistencyCreators from './consistency';
+
+export default {
+  ...DataStructureCreators,
+  ...ConsistencyCreators,
+};
